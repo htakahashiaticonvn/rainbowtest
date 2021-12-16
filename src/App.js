09477afed272662498f54af7c5e57ae8aeff4f05
Hiro Takahashi <takahashih@iconvn.local>
2021-12-16 18:35:32 +0000

    angular error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from './appLogo.png';
-angualr.module('filesStorageService');
+angular.module('filesStorageService');
 import rainbowSDK from 'rainbow-web-sdk';
 import './App.css';
 
